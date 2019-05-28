3fda20b462ae88d3f33c8cc59beb5135c0cfb28a
Nolan Wagner <NolanDubyu@gmail.com>
2019-05-28 11:56:00 -0400

    contact page

--- wp-content/themes/the-ecom-doc/contact-page.php
+++ wp-content/themes/the-ecom-doc/contact-page.php
@@ -3,30 +3,103 @@
 Template Name: Contact
 */
 
-get_header();
+  //response generation function
+
+  $response = "";
+
+  //function to generate response
+  function my_contact_form_generate_response($type, $message){
+
+    global $response;
+
+    if($type == "success") $response = "<div class='success'>{$message}</div>";
+    else $response = "<div class='error'>{$message}</div>";
+
+  }
+
+  //response messages
+  $not_human       = "Human verification incorrect.";
+  $missing_content = "Please supply all information.";
+  $email_invalid   = "Email Address Invalid.";
+  $message_unsent  = "Message was not sent. Try Again.";
+  $message_sent    = "Thanks! Your message has been sent.";
+
+  //user posted variables
+  $name = $_POST['message_name'];
+  $date = $_POST['message_name'];
+  $email = $_POST['message_email'];
+  $message = $_POST['message_date'] . $_POST['message_text'];
+  $human = $_POST['message_human'];
+
+  //php mailer variables
+  $to = get_option('admin_email');
+  $subject = "Someone sent a message from ".get_bloginfo('name');
+  $headers = 'From: '. $email . "\r\n" .
+    'Reply-To: ' . $email . "\r\n";
+
+  if(!$human == 0){
+    if($human != 2) my_contact_form_generate_response("error", $not_human); //not human!
+    else {
+
+      //validate email
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+        my_contact_form_generate_response("error", $email_invalid);
+      else //email is valid
+      {
+        //validate presence of name and message
+        if(empty($name) || empty($message)){
+          my_contact_form_generate_response("error", $missing_content);
+        }
+        else //ready to go!
+        {
+          $sent = wp_mail($to, $subject, strip_tags($message), $headers);
+          if($sent) my_contact_form_generate_response("success", $message_sent); //message sent!
+          else my_contact_form_generate_response("error", $message_unsent); //message wasn't sent
+        }
+      }
+    }
+  }
+  else if ($_POST['submitted']) my_contact_form_generate_response("error", $missing_content);
+
 ?>
+
+<?php get_header(); ?>
 
 
 
 <div class="contact-header">
+<div class="contact-content">
     <h1>Three ways to get in touch.</h1>
     <p>Phasellus vulputate malesuada mauris, vitae ornare nisi. Sed vitae tortor velit. Phasellus eget ante sit amet tellus varius ornare. Etiam vulputate turpis non elit placerat, et posuere lorem ultricies. Pellentesque viverra ante eget egestas fringilla. Nulla at porta leo, nec rutrum augue. Morbi ut libero ut velit feugiat feugiat. Proin nibh neque, maximus eu rhoncus fringilla, egestas ac arcu. Duis ut neque ultrices, ultrices justo sed, rhoncus arcu. Phasellus pellentesque massa vel gravida placerat.
 </p>
-
+</div>
+<div class="contact-method">
+<div id="respond" class="form-container">
+                <?php echo $response; ?>
+                <form class="form" action="<?php the_permalink(); ?>" method="post">
+                  <p><label for="name">Name: <span>*</span> <br><input type="text" name="message_name" value="<?php echo esc_attr($_POST['message_name']); ?>"></label></p>
+                  <p><label for="message_email">Email: <span>*</span> <br><input type="text" name="message_email" value="<?php echo esc_attr($_POST['message_email']); ?>"></label></p>
+                  <p><label for="message_text">Message: <span>*</span> <br><textarea type="text" name="message_text"><?php echo esc_textarea($_POST['message_text']); ?></textarea></label></p>
+                  <p><label for="message_human">Human Verification: <span>*</span> <br><input type="text" style="width: 60px;" name="message_human"> + 3 = 5</label></p>
+                  <input type="hidden" name="submitted" value="1">
+                  <p><input class="contact-submit" type="submit"></p>
+                </form>
+              </div>
+</div>
 </div>
 <div class="contact-container">
-    <div class="content-section">
+    <div class="icon-section">
     <i class="fas fa-comments fa-5x"></i>
-    <p>Etiam a pretium ligula. Nam lacinia dignissim ipsum ut efficitur. Duis id est justo.</p>
+    <p>Coming Soon!</p>
     </div>
-    <div class="content-section">
+    <div class="icon-section">
     <i class="fas fa-phone fa-5x"></i>
-    <p>Etiam a pretium ligula. Nam lacinia dignissim ipsum ut efficitur. Duis id est justo.</p>
+    <p>Call 1 (850) 417 - 4055</p>
     </div>
-    <div class="content-section">
+    <div class="icon-section">
     <i class="fas fa-envelope fa-5x"></i>
 
-    <p>Etiam a pretium ligula. Nam lacinia dignissim ipsum ut efficitur. Duis id est justo.</p>
+    <p>Send a message.</p>
     </div>
     </div>
 
--- wp-content/themes/the-ecom-doc/services-page.php
+++ wp-content/themes/the-ecom-doc/services-page.php
@@ -11,52 +11,43 @@ get_header();
 </div>
 <div>
     <div class="service-section">
-         <i class="fas fa-phone fa-5x margin-1"></i>
+         <i class="fas fa-code fa-3x margin-1"></i>
         <div class="service-content">
             <h2>Theme Design & Development</h2>
             <div class="seperator"></div>
-            <p>Phasellus vulputate malesuada mauris, vitae ornare nisi. Sed vitae tortor velit. Phasellus eget ante sit amet tellus varius ornare. Etiam vulputate turpis non elit placerat, et posuere lorem ultricies. Pellentesque viverra ante eget egestas fringilla. Nulla at porta leo, nec rutrum augue. Morbi ut libero ut velit feugiat feugiat. Proin nibh neque, maximus eu rhoncus fringilla, egestas ac arcu. Duis ut neque ultrices, ultrices justo sed, rhoncus arcu. Phasellus pellentesque massa vel gravida placerat.
-</p>
+            <p>Our team has built 100s of themes for the leading shopping carts, our experience allows us to find the best solutions for your business. Need to add customization to an existing theme? We can do that too.</p>
         </div>  
     </div>
     <div class="service-section">
-         <i class="fas fa-phone fa-5x margin-1"></i>
+         <i class="fas fa-tachometer-alt fa-3x margin-1"></i>
         <div class="service-content">
             <h2>Improve Pagespeed</h2>
             <div class="seperator"></div>
-            <p>Phasellus vulputate malesuada mauris, vitae ornare nisi. Sed vitae tortor velit. Phasellus eget ante sit amet tellus varius ornare. Etiam vulputate turpis non elit placerat, et posuere lorem ultricies. Pellentesque viverra ante eget egestas fringilla. Nulla at porta leo, nec rutrum augue. Morbi ut libero ut velit feugiat feugiat. Proin nibh neque, maximus eu rhoncus fringilla, egestas ac arcu. Duis ut neque ultrices, ultrices justo sed, rhoncus arcu. Phasellus pellentesque massa vel gravida placerat.
-
-</p>
+            <p>What is your stores Google Pagespeed score? This score can directly affect how Google will rank your site. If your ecommerce store takes too long to load customers tend to click the back button, not even viewing your store! Find out your stores page speed score today.</p>
         </div>  
     </div>
     <div class="service-section">
-         <i class="fas fa-phone fa-5x margin-1"></i>
+         <i class="fas fa-mobile-alt fa-3x margin-1"></i>
         <div class="service-content">
-            <h2>Add / Remove Functionality</h2>
+            <h2>Mobile Responsiveness</h2>
             <div class="seperator"></div>
-            <p>Phasellus vulputate malesuada mauris, vitae ornare nisi. Sed vitae tortor velit. Phasellus eget ante sit amet tellus varius ornare. Etiam vulputate turpis non elit placerat, et posuere lorem ultricies. Pellentesque viverra ante eget egestas fringilla. Nulla at porta leo, nec rutrum augue. Morbi ut libero ut velit feugiat feugiat. Proin nibh neque, maximus eu rhoncus fringilla, egestas ac arcu. Duis ut neque ultrices, ultrices justo sed, rhoncus arcu. Phasellus pellentesque massa vel gravida placerat.
-
-</p>
+            <p>If your site isn’t responsive – works on mobile, then your site’s SEO is being negatively affected and over 60% of all users on are mobile devices, that’s over half of your potential customers not seeing your site correctly. Ask us about making your site responsive!</p>
         </div>  
     </div>
     <div class="service-section">
-         <i class="fas fa-phone fa-5x margin-1"></i>
+         <i class="fas fa-shopping-cart fa-3x margin-1"></i>
         <div class="service-content">
             <h2>Shopping Cart Migrations</h2>
             <div class="seperator"></div>
-            <p>Phasellus vulputate malesuada mauris, vitae ornare nisi. Sed vitae tortor velit. Phasellus eget ante sit amet tellus varius ornare. Etiam vulputate turpis non elit placerat, et posuere lorem ultricies. Pellentesque viverra ante eget egestas fringilla. Nulla at porta leo, nec rutrum augue. Morbi ut libero ut velit feugiat feugiat. Proin nibh neque, maximus eu rhoncus fringilla, egestas ac arcu. Duis ut neque ultrices, ultrices justo sed, rhoncus arcu. Phasellus pellentesque massa vel gravida placerat.
-
-</p>
+            <p> Moving your ecommerce store can be an intimidating task and you don’t want to undo any of the hard work that’s already been completed. That’s why it’s important to hire an experienced team to help your store make the move</p>
         </div>  
     </div>
     <div class="service-section">
-         <i class="fas fa-phone fa-5x margin-1"></i>
+         <i class="fas fa-handshake fa-3x margin-1"></i>
         <div class="service-content">
             <h2>Ecommerce Consulting</h2>
             <div class="seperator"></div>
-            <p>Phasellus vulputate malesuada mauris, vitae ornare nisi. Sed vitae tortor velit. Phasellus eget ante sit amet tellus varius ornare. Etiam vulputate turpis non elit placerat, et posuere lorem ultricies. Pellentesque viverra ante eget egestas fringilla. Nulla at porta leo, nec rutrum augue. Morbi ut libero ut velit feugiat feugiat. Proin nibh neque, maximus eu rhoncus fringilla, egestas ac arcu. Duis ut neque ultrices, ultrices justo sed, rhoncus arcu. Phasellus pellentesque massa vel gravida placerat.
-
-</p>
+            <p>Starting out in Ecommerce can be a difficult, confusing task – Which shopping cart is the correct one? Will this be a long term solution for my business? Will I run into any kind of roadblocks with this solution? These are questions we’ve all had when starting with our first ecommerce store, we can make that transition a little easier with our Ecommerce Consultation services.</p>
         </div>  
     </div>
 </div>
