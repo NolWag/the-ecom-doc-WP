bff21644560c46414fe252daec9bcb6e668d13d1
Nolan Wagner <NolanDubyu@gmail.com>
2019-05-30 09:39:27 -0400

    landing page

---
+++ wp-content/themes/the-ecom-doc/landing-page.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Landing Page
+*/
+
+get_header();
+?>
+
+<div class="landing-container">
+    <div class="ecom-doc-container">
+        <img class="ecom-doc-B" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/529582/Ecom-Doc.png" />
+        <div class="econ-doc-platform"></div>
+    </div>
+    <div class="landing-content">
+    <img class="featured-image" src="" />
+    <h1 class="headline">How does your store score?</h1>
+    <p>Lorem ipsum dolor sit amet,
+consectetuer adipiscing elit,
+sed diam nonummy nibh euismod tincidunt ut Lorem ipsum
+dolor sit amet, consectetuer
+adipiscing elit, sed diam
+nonummy nibh euismod tincidunt ut.
+Lorem ipsum dolor sit amet,
+consectetuer adipiscing elit,
+sed diam nonummy nibh euismod tincidunt ut Lorem ipsum
+dolor sit amet.</p>
+<div class="btn btn-gray">Test your store</div>
+<div class="btn btn-red">Speed up store</div>
+</div>
+</div>
+
+<?php get_footer(); ?>
